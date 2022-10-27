6b85300de4dd46884dabd8edc74209a01a01a1ce
Galtozzy <14139502+Galtozzy@users.noreply.github.com>
2022-10-27 18:58:12 +0300

    Removed lazy text

--- health_check/apps.py
+++ health_check/apps.py
@@ -2,14 +2,13 @@
 """Django application config module.
 """
 from django.apps import AppConfig
-from django.utils.translation import ugettext_lazy as _
 
 from health_check.settings import settings
 
 
 class HealthCheck(AppConfig):
     name = 'health_check'
-    verbose_name = _('Health Check')
+    verbose_name = 'Health Check'
 
     def ready(self):
         settings.build_from_django()
